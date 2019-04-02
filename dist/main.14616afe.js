b63ceb2e4d7345a5ab55a3d7f54938fe0392657c
hlnmargary <margary.helene@hetic.net>
2019-04-02 16:37:47 +0200

    scene + object + animation

--- dist/main.14616afe.js
+++ dist/main.14616afe.js
@@ -42419,25 +42419,77 @@ var THREE = _interopRequireWildcard(_three);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+//RERENDER
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+//SCENE
+var scene = new THREE.Scene(); // initialising the scene
+scene.background = new THREE.Color(0xffeccb);
 
-camera.position.z = 5;
+//CAMERA
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+//OBJECT
+function CustomSinCurve(scale) {
+
+  THREE.Curve.call(this);
+
+  this.scale = scale === undefined ? 1 : scale;
+}
+
+CustomSinCurve.prototype = Object.create(THREE.Curve.prototype);
+CustomSinCurve.prototype.constructor = CustomSinCurve;
+
+CustomSinCurve.prototype.getPoint = function (t) {
+
+  var tx = t * 8 - 4.5;
+  var ty = Math.sin(2 * Math.PI * t);
+  var tz = 0;
+
+  return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
+};
+
+var path = new CustomSinCurve(10);
+var geometry = new THREE.TubeGeometry(path, 300, 1, 10, false);
+var material = new THREE.MeshBasicMaterial({ color: 0x31369c });
+var mesh = new THREE.Mesh(geometry, material);
+
+var light = new THREE.AmbientLight(0x404040); // soft white light
+
+
+//ADDES ON SCENE
+scene.add(mesh, light);
+
+// var geometrysphere = new THREE.SphereGeometry(2, 3, 30);
+// var materialsphere = new THREE.MeshBasicMaterial({ color: 0x72369c });
+// var sphere = new THREE.Mesh(geometrysphere, materialsphere);
+
+// var geometrycone = new THREE.ConeGeometry(5, 5, 5);
+// var materialcone = new THREE.MeshBasicMaterial({ color: 0x23369c });
+// var cone = new THREE.Mesh(geometrycone, materialcone);
+
+
+// scene.add(sphere, cone);
+
+function render() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+camera.position.z = 50;
 
 var animate = function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // sphere.rotation.x += 0.00;
+  // sphere.rotation.y += 0.05;
+  // cone.rotation.x += 0.00;
+  // cone.rotation.y += 0.05;
+
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 };
@@ -42472,7 +42524,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '60538' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '63913' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
--- src/main.js
+++ src/main.js
@@ -1,24 +1,84 @@
 import * as THREE from 'three';
-
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+//RERENDER
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
 
-camera.position.z = 5;
+//SCENE
+var scene = new THREE.Scene(); // initialising the scene
+scene.background = new THREE.Color(0xffeccb);
+
+//CAMERA
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+//OBJECT
+function CustomSinCurve(scale) {
+
+  THREE.Curve.call(this);
+
+  this.scale = (scale === undefined) ? 1 : scale;
+
+}
+
+CustomSinCurve.prototype = Object.create(THREE.Curve.prototype);
+CustomSinCurve.prototype.constructor = CustomSinCurve;
+
+CustomSinCurve.prototype.getPoint = function (t) {
+
+  var tx = t * 8 - 4.5;
+  var ty = Math.sin(2 * Math.PI * t);
+  var tz = 0;
+
+  return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
+
+};
+
+var path = new CustomSinCurve(10);
+var geometry = new THREE.TubeGeometry(path, 300, 1, 10, false);
+var material = new THREE.MeshBasicMaterial({ color: 0x31369c });
+var mesh = new THREE.Mesh(geometry, material);
+
+var light = new THREE.AmbientLight(0x404040); // soft white light
+
+
+//ADDES ON SCENE
+scene.add(mesh, light);
+
+
+
+// var geometrysphere = new THREE.SphereGeometry(2, 3, 30);
+// var materialsphere = new THREE.MeshBasicMaterial({ color: 0x72369c });
+// var sphere = new THREE.Mesh(geometrysphere, materialsphere);
+
+// var geometrycone = new THREE.ConeGeometry(5, 5, 5);
+// var materialcone = new THREE.MeshBasicMaterial({ color: 0x23369c });
+// var cone = new THREE.Mesh(geometrycone, materialcone);
+
+
+// scene.add(sphere, cone);
+
+function render() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+
+camera.position.z = 50;
 
 var animate = function () {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // sphere.rotation.x += 0.00;
+  // sphere.rotation.y += 0.05;
+  // cone.rotation.x += 0.00;
+  // cone.rotation.y += 0.05;
+
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+
+
 
   renderer.render(scene, camera);
 };
